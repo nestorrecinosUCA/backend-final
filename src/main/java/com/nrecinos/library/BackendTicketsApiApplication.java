bb44ba7ca6a075ce051057b110f08162eda1896f
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-13 12:10:12 -0600

    :tada: Initialize backend project

---
+++ src/main/java/com/nrecinos/library/BackendTicketsApiApplication.java
@@ -0,0 +1,13 @@
+package com.nrecinos.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTicketsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTicketsApiApplication.class, args);
+	}
+
+}
