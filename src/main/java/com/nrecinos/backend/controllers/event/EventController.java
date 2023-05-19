c118b1d374d7aea5d3861a2f1c6ec74e6420b3c0
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-19 17:57:01 -0600

    :construction: Add validations to create and delete methods

--- src/main/java/com/nrecinos/backend/controllers/event/EventController.java
+++ src/main/java/com/nrecinos/backend/controllers/event/EventController.java
@@ -1,11 +1,20 @@
 package com.nrecinos.backend.controllers.event;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nrecinos.backend.models.dtos.event.CreateEventDto;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/events")
@@ -21,8 +30,11 @@ public class EventController {
 	}
 	
 	@PostMapping("/")
-	String create() {
-		return "Event created";
+	ResponseEntity<?> create(@RequestBody @Valid CreateEventDto createEventDto, BindingResult validations) {
+		if (validations.hasErrors()) {
+			return new ResponseEntity<>(validations.getAllErrors(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
 	@PatchMapping("/{id}")
@@ -31,7 +43,7 @@ public class EventController {
 	}
 	
 	@DeleteMapping("/{id}")
-	String delete() {
-		return "Delete item";
+	ResponseEntity<?> delete(@PathVariable(name = "id") Integer id) {
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
