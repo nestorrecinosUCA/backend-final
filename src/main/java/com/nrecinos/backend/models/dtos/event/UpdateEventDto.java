96ad3e1ff57599c8445143cb7e675557c8c2972b
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-05-19 18:01:54 -0600

    :safety_vest: Add validations for updateEventDto and update createEventDto

--- src/main/java/com/nrecinos/backend/models/dtos/event/UpdateEventDto.java
+++ src/main/java/com/nrecinos/backend/models/dtos/event/UpdateEventDto.java
@@ -10,39 +10,30 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class CreateEventDto {
+public class UpdateEventDto {
 	@Size(max = 50, message = "Title is bigger than allowed")
-	@NotEmpty(message = "title must not be empty")
 	String title;
 
 	@Size(max = 200, message = "Description is bigger than allowed")
-	@NotEmpty(message = "description must not be empty")
 	String description;
 
-	@NotEmpty(message = "date must not be empty")
 	Date date;
-
-	@NotEmpty(message = "hour must not be empty")
+	
+	// Add a regex to validate the hour
 	String hour;
 
-	@NotEmpty(message ="duration must not be empty")
 	Float duration;
 	// The isActive field is not created because it is true by default
-
-	@NotEmpty(message = "assistantCapacity must not be empty")
+ 
 	@Positive()
 	Integer assistantsCapacity;
 
-	@NotEmpty(message = "userId must not be empty")
 	@Positive()
 	Integer userId;
 
-	@NotEmpty(message = "tierId must not be empty")
 	@Positive()
 	Integer tierId;
-
-	@NotEmpty(message = "categoryId must not be empty")
+	
 	@Positive()
 	Integer categoryId;
-
 }
