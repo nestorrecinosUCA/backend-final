fb5578ee2114e2f3c8ec2693f4a3eaa85d8fd0cc
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-06-26 21:20:05 -0600

    :card_file_box: Update repository for sponsors

--- backend-api/src/main/java/com/nrecinos/backend/repositories/SponsorRepository.java
+++ backend-api/src/main/java/com/nrecinos/backend/repositories/SponsorRepository.java
@@ -1,10 +1,14 @@
 package com.nrecinos.backend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nrecinos.backend.models.entities.category.Category;
 import com.nrecinos.backend.models.entities.sponsor.Sponsor;
 
 public interface SponsorRepository extends JpaRepository<Sponsor, Integer>{
 	Sponsor findOneById(Integer id);
+    List<Sponsor> findAllByEventId(Integer eventId);
 }
