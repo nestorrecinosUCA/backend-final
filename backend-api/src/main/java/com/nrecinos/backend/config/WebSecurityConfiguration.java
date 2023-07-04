29920621a63c33ad7754e2e6664db4ef426d7835
Nestor Recinos <nestor.recinos@airlinemro.parts>
2023-07-04 09:34:05 -0600

    Update access to events

--- backend-api/src/main/java/com/nrecinos/backend/config/WebSecurityConfiguration.java
+++ backend-api/src/main/java/com/nrecinos/backend/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.nrecinos.backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -61,6 +62,7 @@ public class WebSecurityConfiguration {
 	    http.authorizeHttpRequests(auth -> 
 	    	auth
 	    		.requestMatchers("/auth/**").permitAll()
+	    		.requestMatchers(HttpMethod.GET, "/events").permitAll()
 	    		.anyRequest().authenticated()
 	    );
 	    
